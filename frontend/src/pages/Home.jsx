01694c1ef9b9e67becb976c7088d40df5993e355
abhiroy00 <abhi7464099@gmail.com>
2025-01-15 00:28:30 +0530

    change stucre
    change stucre

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,34 +1,56 @@
+import { useState, useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../comman/Header';
 import Footer from '../comman/Footer';
-import { Outlet } from 'react-router-dom';
 import About from '../features/about/About';
 import Course from '../features/courses/Course';
 import Solutions from '../features/new_ solutions/Solutions';
+import microsoftpng from '../assets/icons/microsoft.png'
+import  down from '../assets/icons/dwn.png'
+import coconut from '../assets/icons/coconut.png'
 
 export default function Home() {
+  const [showWrapper, setShowWrapper] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    // Hide the wrapper on certain routes (e.g., when on '/about', '/courses', '/solutions')
+    if (location.pathname === '/about' || location.pathname === '/course' || location.pathname === '/solutions') {
+      setShowWrapper(false);
+    } else {
+      setShowWrapper(true);
+    }
+  }, [location]);
+
   return (
     <div>
       <Header />
-      <Outlet></Outlet>
-      <div
-      >
-        <div className='wrapper'>
-        <div class="content">
-<div className='box'>
-                <div className="content-img">
-                    <img src="img/icons/microsoft.png"></img>
-                </div>
-                <h1 className="heading">Microsoft Solutions<span
-                        class="head-clr">Dynamics CRM . Azure . Office
-                        365</span></h1>
+
+      {/* Outlet for nested routing */}
+      <Outlet />
+
+      {showWrapper && (
+        <div className="wrapper">
+          <div className="content">
+            <div className="box">
+              <div className="content-img">
+                <img src={microsoftpng } alt="Microsoft Solutions Logo" />
+              </div>
+              <h1 className="heading">
+                Microsoft Solutions
+                <span className="head-clr">
+                  Dynamics CRM . Azure . Office 365
+                </span>
+              </h1>
             </div>
-            </div>
-            </div>
-        
-      </div>
-      <About/>
-      <Course/>
-      <Solutions/>
+          </div>
+        </div>
+      )}
+
+      {/* Sections for About, Courses, and Solutions */}
+      <About />
+      <Course />
+      <Solutions />
 
       <Footer />
     </div>
