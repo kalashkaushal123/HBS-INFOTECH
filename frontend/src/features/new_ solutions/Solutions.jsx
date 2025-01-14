c95e5c16c5d86cb76aeb81a6e418955193e1052e
abhiroy00 <abhi7464099@gmail.com>
2025-01-14 20:38:54 +0530

    second commit
    second commit

--- frontend/src/features/new_ solutions/Solutions.jsx
+++ frontend/src/features/new_ solutions/Solutions.jsx
@@ -1,156 +1,133 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import './solution.css'
+import './solutions.css'
 
 export default function Solutions() {
   return (
-    <div className="container my-5">
-      <div class="Solutions mt-60">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 line">
-                    <div class="inner-text"></div>
-                    <h2 class="head">Bridge the Gap</h2>
-                    <p class="pera">Between Business and Technology</p>
-                </div>
-                <div class="col-md-3 line">
-                    <div class="inner-text">
-                        <h3 class="sub-head pink">
-                            01
-                        </h5>
-                        <h4 class="small-head">Microsoft Solutions</p>
-                        <p>Unlock the power of Microsoft's suite of solutions
-                            and elevate your business. Streamline
-                            operations, boost collaboration, and drive
-                            innovation with our expertly tailored
-                            Microsoft services.
-                        </p>
-                        <div class="btn"><a class="learn-more"
-                                href="/microsoft-solutions/ ">Learn
-                                More</a></div>
-                    </div>
-                </div>
-                <div class="col-md-3 line">
-                    <div class="inner-text">
-                        <h3 class="sub-head sky">
-                            02
-                        </h5>
-                        <h4 class="small-head">Coconut ERP
-                        </p>
-                        <p>With Coconut ERP you get Industry 4.0 compatible ERP.
-                            Now you do have visibility of all
-                            of your plants and sites with single intelligent and
-                            smart age dashboard.
-
-                        </p>
-                        <div class="btn"><a class="learn-more"
-                                href="/microsoft-solutions/ ">Learn
-                                More</a></div>
-                    </div>
-                </div>
-                <div class="col-md-3 ">
-                    <div class="inner-text">
-                        <h3 class="sub-head yellow ">
-                            03
-                        </h5>
-                        <h4 class="small-head">atsMantra
-
-                        </p>
-                        <p>Case is defined in businesses as one of issues to be
-                            handled. With Caseio Enterprises can
-                            resolve complex issues and ensure that optimal
-                            solutions are put together.
-
-                        </p>
-                        <div class="btn"><a class="learn-more"
-                                href="/microsoft-solutions/ ">Learn
-                                More</a></div>
-                    </div>
-                </div>
+    <>
+      <div className="Solutions mt-60">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3 line">
+              <div className="inner-text"></div>
+              <h2 className="head">Bridge the Gap</h2>
+              <p className="pera">Between Business and Technology</p>
             </div>
-            <div class="horig">
-                <div class="horig-line"></div>
+            <div className="col-md-3 line">
+              <div className="inner-text">
+                <h3 className="sub-head pink">
+                  01
+                </h3> {/* Fixed mismatched closing tag */}
+                <h4 className="small-head">Microsoft Solutions</h4> {/* Fixed mismatched closing tag */}
+                <p>Unlock the power of Microsoft's suite of solutions
+                  and elevate your business. Streamline
+                  operations, boost collaboration, and drive
+                  innovation with our expertly tailored
+                  Microsoft services.
+                </p>
+                <div className="btn"><a className="learn-more"
+                  href="/microsoft-solutions/">Learn More</a></div>
+              </div>
             </div>
-            <div class="row">
-                <div class="col-md-3 line">
-                    <div class="inner-text">
-                        <h3 class="sub-head green">
-                            04
-                        </h5>
-                        <h4 class="small-head">Recruitment Services
-
-                        </p>
-                        <p>Unlock the power of Microsoft's suite of solutions
-                            and elevate your business. Streamline
-                            operations, boost collaboration, and drive
-                            innovation with our expertly tailored
-                            Microsoft services.
-                        </p>
-                        <div class="btn"><a class="learn-more"
-                                href="/microsoft-solutions/ ">Learn
-                                More</a></div>
-                    </div>
-                </div>
-                <div class="col-md-3 line">
-                    <div class="inner-text">
-                        <h3 class="sub-head purpal">
-                            05
-                        </h5>
-                        <h4 class="small-head">Application
-                            development
-
-                        </p>
-                        <p>Unlock the power of Microsoft's suite of solutions
-                            and elevate your business. Streamline
-                            operations, boost collaboration, and drive
-                            innovation with our expertly tailored
-                            Microsoft services.
-                        </p>
-                        <div class="btn"><a class="learn-more"
-                                href="/microsoft-solutions/ ">Learn
-                                More</a></div>
-                    </div>
-                </div>
-                <div class="col-md-3 line">
-                    <div class="inner-text">
-                        <h3 class="sub-head yellow">
-                            06
-                        </h5>
-                        <h4 class="small-head">UI/UX Design
-
-                        </p>
-                        <p>With Coconut ERP you get Industry 4.0 compatible ERP.
-                            Now you do have visibility of all
-                            of your plants and sites with single intelligent and
-                            smart age dashboard.
-
-                        </p>
-                        <div class="btn"><a class="learn-more"
-                                href="/microsoft-solutions/ ">Learn
-                                More</a></div>
-                    </div>
-                </div>
-                <div class="col-md-3 ">
-                    <div class="inner-text">
-                        <h3 class="sub-head sky ">
-                            07
-                        </h5>
-                        <h4 class="small-head">atsMantra
-
-                        </p>
-                        <p>Case is defined in businesses as one of issues to be
-                            handled. With Caseio Enterprises can
-                            resolve complex issues and ensure that optimal
-                            solutions are put together.
-
-                        </p>
-                        <div class="btn"><a class="learn-more"
-                                href="/microsoft-solutions/ ">Learn
-                                More</a></div>
-                    </div>
-                </div>
+            <div className="col-md-3 line">
+              <div className="inner-text">
+                <h3 className="sub-head sky">
+                  02
+                </h3> {/* Fixed mismatched closing tag */}
+                <h4 className="small-head">Coconut ERP</h4> {/* Fixed mismatched closing tag */}
+                <p>With Coconut ERP you get Industry 4.0 compatible ERP.
+                  Now you do have visibility of all
+                  of your plants and sites with a single intelligent and
+                  smart age dashboard.
+                </p>
+                <div className="btn"><a className="learn-more"
+                  href="/microsoft-solutions/">Learn More</a></div>
+              </div>
             </div>
+            <div className="col-md-3">
+              <div className="inner-text">
+                <h3 className="sub-head yellow">
+                  03
+                </h3> {/* Fixed mismatched closing tag */}
+                <h4 className="small-head">atsMantra</h4> {/* Fixed mismatched closing tag */}
+                <p>Case is defined in businesses as one of issues to be
+                  handled. With Caseio Enterprises can
+                  resolve complex issues and ensure that optimal
+                  solutions are put together.
+                </p>
+                <div className="btn"><a className="learn-more"
+                  href="/microsoft-solutions/">Learn More</a></div>
+              </div>
+            </div>
+          </div>
+          <div className="horig">
+            <div className="horig-line"></div>
+          </div>
+          <div className="row">
+            <div className="col-md-3 line">
+              <div className="inner-text">
+                <h3 className="sub-head green">
+                  04
+                </h3> {/* Fixed mismatched closing tag */}
+                <h4 className="small-head">Recruitment Services</h4> {/* Fixed mismatched closing tag */}
+                <p>Unlock the power of Microsoft's suite of solutions
+                  and elevate your business. Streamline
+                  operations, boost collaboration, and drive
+                  innovation with our expertly tailored
+                  Microsoft services.
+                </p>
+                <div className="btn"><a className="learn-more"
+                  href="/microsoft-solutions/">Learn More</a></div>
+              </div>
+            </div>
+            <div className="col-md-3 line">
+              <div className="inner-text">
+                <h3 className="sub-head purple"> {/* Fixed typo in class name */}
+                  05
+                </h3> {/* Fixed mismatched closing tag */}
+                <h4 className="small-head">Application Development</h4> {/* Fixed mismatched closing tag */}
+                <p>Unlock the power of Microsoft's suite of solutions
+                  and elevate your business. Streamline
+                  operations, boost collaboration, and drive
+                  innovation with our expertly tailored
+                  Microsoft services.
+                </p>
+                <div className="btn"><a className="learn-more"
+                  href="/microsoft-solutions/">Learn More</a></div>
+              </div>
+            </div>
+            <div className="col-md-3 line">
+              <div className="inner-text">
+                <h3 className="sub-head yellow">
+                  06
+                </h3> {/* Fixed mismatched closing tag */}
+                <h4 className="small-head">UI/UX Design</h4> {/* Fixed mismatched closing tag */}
+                <p>With Coconut ERP you get Industry 4.0 compatible ERP.
+                  Now you do have visibility of all
+                  of your plants and sites with a single intelligent and
+                  smart age dashboard.
+                </p>
+                <div className="btn"><a className="learn-more"
+                  href="/microsoft-solutions/">Learn More</a></div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="inner-text">
+                <h3 className="sub-head sky">
+                  07
+                </h3> {/* Fixed mismatched closing tag */}
+                <h4 className="small-head">atsMantra</h4> {/* Fixed mismatched closing tag */}
+                <p>Case is defined in businesses as one of issues to be
+                  handled. With Caseio Enterprises can
+                  resolve complex issues and ensure that optimal
+                  solutions are put together.
+                </p>
+                <div className="btn"><a className="learn-more"
+                  href="/microsoft-solutions/">Learn More</a></div>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
-    </div>
+      </div>
+    </>
   );
 }
