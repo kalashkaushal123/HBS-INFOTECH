5c8db289ce5723f0cb452f3a3b5b77a093fc0907
abhiroy00 <abhi7464099@gmail.com>
2025-01-21 14:50:14 +0530

    abhi commit2
    abhi commit2

--- frontend/src/features/secoundHeader/SecoundHeader.jsx
+++ frontend/src/features/secoundHeader/SecoundHeader.jsx
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
 import React, { useState } from 'react'
-=======
-
->>>>>>> Stashed changes
 import './SecoundHeader.css'
 import { Link } from 'react-router-dom';
 const SecoundHeader = () => {
@@ -104,7 +100,6 @@ const SecoundHeader = () => {
             <div className="row m-0 p-0">
                 {/* <div className="col-md-2 p-0 m-0"></div> <div className="col-md-1"></div> */}
 
-<<<<<<< Updated upstream
                 {state.map((item,index)=>
                 <>
                 <div className="col m-0 p-0 " onClick={showelement}>
@@ -118,177 +113,14 @@ const SecoundHeader = () => {
                             </label>
                         </span> 
                     </div>
-=======
-        <div align='center' style={{marginLeft:"4rem",display:"flex",gap:"12rem"}}>
-
-          <div className="top-second-header">
-          <div>
-          <Link to="/course"><button className="btn btn-danger custom-btn">Certification with HBS </button></Link>
-          </div>
-          </div>
-       <div className="bottom-second-header d-flex ">
-       <div className="" onClick={showelement} style={{marginLeft:"2rem"}}>
-            <img width={'25px'} src="src/features/secoundHeader/img/doctorateactive.svg"  alt="Doctorate"/><br />
-            <span>Doctorate</span>
-        </div>
-        <div className="" onClick={showelement} style={{marginLeft:"2rem"}}>
-            <img width={'25px'} src="src\features\secoundHeader\img\AI+active.svg" alt="AI & ML"/><br />
-            <span>AI & ML</span>
-        </div>
-        <div className="" onClick={showelement} style={{marginLeft:"2rem"}}>
-            <img width={'25px'} src="src\features\secoundHeader\img\MBA+active.svg" alt="MBA"/><br />
-            <span>MBA</span>
-        </div>
-        <div className="" onClick={showelement} style={{marginLeft:"2rem"}}>
-            <img width={'25px'} src="src\features\secoundHeader\img\data+science+active.svg" alt="Data Science"/><br />
-            <span>Data Science</span>
-        </div>
-        <div className="" onClick={showelement} style={{marginLeft:"2rem"}}>
-            <img width={'25px'} src="src\features\secoundHeader\img\Marketing+active.svg" alt="Marketing"/><br />
-            <span>Marketing</span>
-        </div>
-        <div className="" onClick={showelement} style={{marginLeft:"2rem"}}>
-            <img width={'25px'} src="src\features\secoundHeader\img\management+active.svg" alt="Management"/><br />
-            <span>Management</span>
-        </div>
-        <div className="" onClick={showelement} style={{marginLeft:"2rem"}}>
-            <img width={'25px'} src="src\features\secoundHeader\img\technology+active.svg" alt="Software & Tech"/><br />
-            <span>Software & Tech</span>
-        </div>
-        <div className="" onClick={showelement} style={{marginLeft:"2rem"}}>
-            <img width={'25px'} src="src\features\secoundHeader\img\Law+Active.svg" alt="Law"/><br />
-            <span>Law</span>
-        </div>
-
-       </div>
-
-        </div>
-
-        <div className='' id='aaa' style={{width:"80%",height:"200px", display:'none'}}>
-               <div className="row bg-white" style={{borderBottomRightRadius:'2rem',borderBottomLeftRadius:'2rem'}}>
-                <div className="col-md-3 mt-3">
-                    <button className='btn btn-success'>Degree / Exec. PG  
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16"><path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/></svg></button><br /><br />
-                    <button className='btn btn-success'>Executive Certificate 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16"><path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/></svg> </button>
->>>>>>> Stashed changes
                 </div>
                
                 </>
                 )
 
-<<<<<<< Updated upstream
                 }
             </div>
         
-=======
-                      <div className="col-md-6">
-                          <div className="mb-3" >
-
-                                  <div className="row g-0">
-                                    <div className="col-md-1">
-                                      <img width='40px'src="https://d2o2utebsixu4k.cloudfront.net/upgrad/uni-cards-logos/DS_UniLogo_Dweb/DS_ProgramLogo_Dweb_IITB.svg" alt="" className=" border img-fluid rounded-start" />
-                                    </div>
-                                    <div className="col-md-8">
-                                      <div className="card-body">
-                                        <h5 className="card-title " style={{fontSize:"10px"}}>IIIT Bangalore</h5>
-                                        <p className="card-text" style={{fontSize:"10px"}}>Executive Program in Generative AI for Leaders</p>
-                                        {/* <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> */}
-                                      </div>
-                                    </div>
-                                  </div>
-
-                          </div>
-                      </div>
-
-                      <div className="col-md-6">
-                          <div className="mb-3" >
-
-                                  <div className="row g-0">
-                                    <div className="col-md-1">
-                                      <img width='40px'src="https://d2o2utebsixu4k.cloudfront.net/upgrad/uni-cards-logos/DS_UniLogo_Dweb/DS_ProgramLogo_Dweb_IITB.svg" alt="" className=" border img-fluid rounded-start" />
-                                    </div>
-                                    <div className="col-md-8">
-                                      <div className="card-body">
-                                        <h5 className="card-title " style={{fontSize:"10px"}}>IIIT Bangalore</h5>
-                                        <p className="card-text" style={{fontSize:"10px"}}>Post Graduate Certificate in Machine Learning and Deep Learning (Executive)</p>
-                                        {/* <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> */}
-                                      </div>
-                                    </div>
-                                  </div>
-
-                          </div>
-                      </div>
-
-
-                      <div className="col-md-6">
-                          <div class="mb-3" >
-
-                                  <div class="row g-0">
-                                    <div class="col-md-1">
-                                      <img width='40px'src="http://d2o2utebsixu4k.cloudfront.net/upgrad/new-home/svg/upGrad.svg" alt="" className=" border img-fluid rounded-start" />
-                                    </div>
-                                    <div class="col-md-8">
-                                      <div class="card-body">
-                                        <h5 class="card-title " style={{fontSize:"10px"}}>upGrad</h5>
-                                        <p class="card-text" style={{fontSize:"10px"}}>Advanced Certificate Program in Generative AI</p>
-                                        {/* <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> */}
-                                      </div>
-                                    </div>
-                                  </div>
-
-                          </div>
-                      </div>
-
-
-                      <div className="col-md-6">
-                          <div class="mb-3" >
-
-                                  <div class="row g-0">
-                                    <div class="col-md-1">
-                                      <img width='40px'src="https://d2o2utebsixu4k.cloudfront.net/upgrad/logos/U%26AI_Logo.svg" alt="" className="bg-white border img-fluid rounded-start" />
-                                    </div>
-                                    <div class="col-md-8">
-                                      <div class="card-body">
-                                        <h5 class="card-title " style={{fontSize:"10px"}}>upGrad | Microsoft</h5>
-                                        <p class="card-text" style={{fontSize:"10px"}}>The U & AI GenAI Certificate Program from Microsoft
-</p>
-                                        {/* <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> */}
-                                      </div>
-                                    </div>
-                                  </div>
-
-                          </div>
-                      </div>
-
-
-                      <div className="col-md-6">
-                          <div class="mb-3" >
-
-                                  <div class="row g-0">
-                                    <div class="col-md-1">
-                                      <img width='40px'src="https://d2o2utebsixu4k.cloudfront.net/upgrad/uni-cards-logos/DS_UniLogo_Dweb/DS_ProgramLogo_Dweb_IITB.svg" alt="" className=" border img-fluid rounded-start" />
-                                    </div>
-                                    <div class="col-md-8">
-                                      <div class="card-body">
-                                        <h5 class="card-title " style={{fontSize:"10px"}}>IIIT Bangalore</h5>
-                                        <p class="card-text" style={{fontSize:"10px"}}>Post Graduate Certificate in Machine Learning & NLP (Executive)</p>
-                                        {/* <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> */}
-                                      </div>
-                                    </div>
-                                  </div>
-
-                          </div>
-                      </div>
-                      
-
-
-
-                       
-                       </div>
-                </div>
-               </div>
->>>>>>> Stashed changes
         </div>
 
 
