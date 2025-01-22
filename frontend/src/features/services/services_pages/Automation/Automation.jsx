047c47cb35b6d68992503ffbf7531a89effb1d90
kalashkaushal123 <kalashkaushal664@gmail.com>
2025-01-22 14:18:44 +0530

    automation

--- frontend/src/features/services/services_pages/Automation/Automation.jsx
+++ frontend/src/features/services/services_pages/Automation/Automation.jsx
@@ -1,9 +1,360 @@
 import './automation.css'
 
+import main_image from './image/main.webp'
+import setting from './image/setting.png'
+
+import functional_testing from './image/functional_testing.png'
+import automated_testing from './image/automated_testing.png'
+import security_testing from './image/security_testing.png'
+import dedicated_term from './image/dedicated_term.png'
+import manual_test from './image/manual_test.png'
+import performance from './image/performance.png'
+import manage from './image/manage.png'
+import data_test from './image/data_test.png'
+
+import mobile from './image/mobile.png'
+import orange from './image/orange.png'
+import dvs from './image/dvs.png'
+import openBet from './image/openBet.png'
+
+import case1 from './image/case1.png'
+import case2 from './image/case2.png'
+
+import card1 from './image/card1.webp'
+
+
 export default function Automation() {
   return (
     <div>
-      <h1>hii am Automation</h1>
+    <div className='main_div'>
+      <img src={main_image} alt="" className='main_image' />
+      <div className="gradient"></div>
+      <div className='main_text'>
+        <img src={setting} alt="" />
+        <h1>Automation Testing Services</h1>
+        <p>Improve accuracy and speed up software delivery through automated testing strategies that save time, reduce costs and enhance product quality.</p>
+        <button>Contact us</button>
+      </div>
+    </div>
+
+    <div className='auto'>
+      <div className='auto-text'>
+        <h2>
+          Automation testing services increase accuracy & speed up delivery
+        </h2>
+        <p>
+          For companies serious about software quality and committed to swiftly delivering services to their customers, automatic tests are essential. We provide comprehensive support in the construction of integrated test platforms, including production environments, test teams and the tests themselves.
+        </p>
+      </div>
+      
+      <h2 style={{fontWeight:"bold", fontSize:'36px'}}>Our testing services cover all stages of software development</h2>
+
+      <div className='testing'>
+        <div>
+
+          <div className='testing_block'>
+            <div className='testing_image'>
+              <img src={functional_testing} alt="" height="60px" />
+            </div>
+            <div className='testing_text'>
+              <h5 style={{fontWeight:'600'}}>Functional Testing</h5>
+              <p>Provide full functionality of your software to meet your customers’ needs and increase customer trust in your product.</p>
+            </div>
+          </div>
+
+          <div className='testing_block'>
+            <div className='testing_image'>
+              <img src={automated_testing} alt="" height="60px"  />
+            </div>
+            <div className='testing_text'>
+              <h5 style={{fontWeight:'600'}}>Automated Testing</h5>
+              <p>Automate critical application areas or build end-to-end test automation platforms using the latest tools.</p>
+            </div>
+          </div>
+
+          <div className='testing_block'>
+            <div className='testing_image'>
+              <img src={security_testing} alt="" height="60px"  />
+            </div>
+            <div className='testing_text'>
+              <h5 style={{fontWeight:'600'}}>Security Testing</h5>
+              <p>Carry out environmental safety audits and protect your customers’ data.</p>
+            </div>
+          </div>
+
+          <div className='testing_block'>
+            <div className='testing_image'>
+              <img src={dedicated_term} alt="" height="60px"  />
+            </div>
+            <div className='testing_text'>
+              <h5 style={{fontWeight:'600'}}>Dedicated team or end-to-end service</h5>
+              <p>Build a dedicated team of carefully selected specialists or deliver our service in a Fix Price model.</p>
+            </div>
+          </div>
+
+        </div>
+        <div>
+
+          <div className='testing_block'>
+            <div className='testing_image'>
+              <img src={manual_test} alt="" height="60px"  />
+            </div>
+            <div className='testing_text'>
+              <h5 style={{fontWeight:'600'}}>Manual Testing</h5>
+              <p>Test whether new functionalities comply with requirements, deliver a rewarding interface and are intuitive to use.</p>
+            </div>
+          </div>
+
+          <div className='testing_block'>
+            <div className='testing_image'>
+              <img src={performance} alt="" height="60px"  />
+            </div>
+            <div className='testing_text'>
+              <h5 style={{fontWeight:'600'}}>Performance Testing</h5>
+              <p>Identify application performance problems (slow response times, bottlenecks and poor scalability) before running your solution in a production environment.</p>
+            </div>
+          </div>
+
+          <div className='testing_block'>
+            <div className='testing_image'>
+              <img src={manage} alt="" height="60px"  />
+            </div>
+            <div className='testing_text'>
+              <h5 style={{fontWeight:'600'}}>Managed Testing</h5>
+              <p>Manage your test processes so you can focus on your core business.</p>
+            </div>
+          </div>
+
+          <div className='testing_block'>
+            <div className='testing_image'>
+              <img src={data_test} alt="" height="60px"  />
+            </div>
+            <div className='testing_text'>
+              <h5 style={{fontWeight:'600'}}>Test Data Management</h5>
+              <p>Create and manage test data in an efficient and automated manner.</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <h2>Why do companies turn to us?</h2>
+
+      <div className='para'>
+        <div>
+
+          <div className='com'>
+            <div className='com_icon'></div>
+            <div className='com-text'>
+              <h5 style={{fontWeight:'600'}}>Extensive experience</h5>
+              <p>across the financial services, telecommunications, sports betting and other industries</p>
+            </div>
+          </div>
+
+          <div className='com'>
+            <div className='com_icon'></div>
+              <div className='com-text'>
+                <h5 style={{fontWeight:'600'}}>Extensive experience</h5>
+                <p>across the financial services, telecommunications, sports betting and other industries</p>
+            </div>
+          </div>
+
+          <div className='com'>
+            <div className='com_icon'></div>
+              <div className='com-text'>
+                <h5 style={{fontWeight:'600'}}>Extensive experience</h5>
+                <p>across the financial services, telecommunications, sports betting and other industries</p>
+              </div>
+            </div>
+        </div>
+ 
+        <div style={{marginLeft:'80px'}}>
+
+          <div className='com'>
+            <div className='com_icon'></div>
+            <div className='com-text'>
+              <h5 style={{fontWeight:'600'}}>Extensive experience</h5>
+              <p>across the financial services, telecommunications, sports betting and other industries</p>
+            </div>
+          </div>
+
+          <div className='com'>
+            <div className='com_icon'></div>
+            <div className='com-text'>
+              <h5 style={{fontWeight:'600'}}>Extensive experience</h5>
+              <p>across the financial services, telecommunications, sports betting and other industries</p>
+            </div>
+          </div>
+
+          <div className='com'>
+            <div className='com_icon'></div>
+            <div className='com-text'>
+              <h5 style={{fontWeight:'600'}}>Extensive experience</h5>
+              <p>across the financial services, telecommunications, sports betting and other industries</p>
+            </div>
+          </div>
+        </div>
+ 
+      </div>
+      </div>
+      
+      <section className='device'>
+        <h2>Who we’ve helped</h2>
+
+        <div>
+          <img src={mobile} alt="" height='50px' />
+          <img src={orange} alt="" height='70px' className='dev_img' />
+          <img src={dvs} alt="" height='35px' className='dev_img' />
+          <img src={openBet} alt="" height='40px' className='dev_img' />
+        </div>
+      </section>
+
+      <section className='auto'>
+        <div className='case_head'>
+          <h2>Case studies</h2>
+          <button>More case studies</button>
+        </div>
+
+        <div className='case_box'>
+          <div className='case_img'>
+            <img src={case1} alt="" />
+          </div>
+          <div className='case_text'>
+            <a href="">
+              <p style={{marginBottom:'10px'}}>FANANCIAL SERVICES</p>
+              <h3>Building a test automation platform for a mulinational insurance company</h3>
+              <p>Software Mind prepared a test automation framework based on Selenium WebDriver and created robots to automate labor-intensive and repetitive work, automated web and desktop applications, as well as installed and configured a test suite launching platform.</p>
+              <button>Learn more</button>
+            </a>
+          </div>
+        </div>
+
+      
+        <div className='case_box box1'>
+          <div className='case_img'>
+            <img src={case2} alt="" />
+          </div>
+          <div className='case_text1'>
+            <a href="">
+              <p style={{marginBottom:'10px'}}>Telecom</p>
+              <h3>Building and developing a test automation platform for an international mobile telecommunications company </h3>
+              <p>Software Mind helped a global telco company build a test automation platform for physical and virtual machines. Beyond enabling reporting on various platforms and developing a GUI to access data, we migrated and rebuilt existing automated scripts. </p>
+              <button>Learn more</button>
+            </a>
+          </div>
+        </div>
+
+
+        <div className='auto_box'>
+          <h2>
+            Automation testing insights
+          </h2>
+          <p>
+            Get expert advice and best practices on efficiently testing solutions, automating tests and conducting effective quality assurance.
+          </p>
+        </div>
+
+
+
+        <div className='auto_box'>
+          <h2 className='auto_box_heading'>
+            Looking for other software services?
+          </h2>
+          <p>
+            For over two decades we’ve been helping companies across markets and sectors develop disruptive solutions. Proven ways of working, domain knowledge and an open culture that prioritizes ownership mean we contribute from day one.
+          </p>
+        </div>
+
+
+        <h2 style={{marginTop:'100px', marginBottom:'60px'}}>Engineering and consultancy that deliver value</h2>
+
+
+
+        <div style={{display:'flex'}}>
+          <div className='gen'> 
+            <img src={card1} alt="" height='300px' />
+            <div className='gen_text'>
+              Generative AI development services
+            </div>
+          
+            <div className='text'>
+              Use generative AI models to stay ahead of your competition.
+            </div>
+          </div>
+
+          <div className='gen'> 
+            <img src={card1} alt="" height='300px' />
+            <div className='gen_text'>
+              Cloud consulting & services
+            </div>
+          
+            <div className='text'>
+              Use generative AI models to stay ahead of your competition.
+            </div>
+          </div>
+
+          <div className='gen'> 
+            <img src={card1} alt="" height='300px' />
+            <div className='gen_text'>
+            Data science services
+            </div>
+          
+            <div className='text'>
+            Operationalize data to drive efficiency, insights and decision-making. 
+            </div>
+          </div>
+        </div>
+
+        
+
+
+        <h2 style={{marginTop:'100px', marginBottom:'60px'}}>Engineering and consultancy that deliver value</h2>
+
+
+
+        <div style={{display:'flex'}}>
+          <div className='gen'> 
+            <img src={card1} alt="" height='300px' />
+            <div className='gen_text'>
+            Financial services
+            </div>
+          
+            <div className='text'>
+            Engineer customized solutions that increase personalization and user conversion across channels. 
+            </div>
+          </div>
+
+          <div className='gen'> 
+            <img src={card1} alt="" height='300px' />
+            <div className='gen_text'>
+            Telecom
+            </div>
+          
+            <div className='text'>
+            Work with experienced engineering teams to create evolutive solutions for your customers. 
+            </div>
+          </div>
+
+          <div className='gen'> 
+            <img src={card1} alt="" height='300px' />
+            <div className='gen_text'>
+            Sports betting
+            </div>
+          
+            <div className='text'>
+            Develop online betting software that prioritizes rewarding customer experience. 
+            </div>
+          </div>
+        </div>
+
+
+
+
+      </section>
+
+
+    
+      
     </div>
   )
 }
