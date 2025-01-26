c0ddfd97573f49222eb2b62a2bd64933f385466b
abhiroy00 <abhi7464099@gmail.com>
2025-01-26 23:24:43 +0530

    i added changes in header
    i added changes in header

--- frontend/src/comman/Header.jsx
+++ frontend/src/comman/Header.jsx
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom';
 import './header.css';
 import { useState } from 'react'; 
 import { FaAlignJustify } from "react-icons/fa";
+
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isHoveringServices, setIsHoveringServices] = useState(false); 
+  const [isHoveringIndustries, setIsHoveringIndustries] = useState(false);
+  const [isHoveringProduct, setIsHoveringProduct] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen); 
   };
+
   return (
     <div>
       <header className="py-3">
@@ -56,86 +60,116 @@ export default function Header() {
                 {/* Conditionally show the dropdown */}
                 <div className={`services-dropdown ${isHoveringServices ? 'show' : ''}`}>
                   <div className="services-menu">
-                  <div className="services-left-side">
-                    <h1 className='m-5'>AI expertise tailored to business goals </h1>
-                    <button className="btn btn-danger m-5" style={{backgroundColor:"red",
-                      position:"relative",
-                      top:"100px",
-                      left:"20px",
-                      height:"4rem",
-                      width:"20rem",
-                      fontSize:"25px",
-                      fontWeight:"bolder"
-                    }}>Lets together work</button>
+                    <div className="services-left-side" style={{position:"relative",left:"30px"}}>
+                      <h1 className='m-5'>AI expertise tailored to business goals </h1>
+                      <button style={{
+                        backgroundColor:"red",
+                        position:"relative",
+                        top:"100px",
+                        left:"55px",
+                        height:"4rem",
+                        width:"20rem",
+                        fontSize:"25px",
+                        fontWeight:"bolder",
+                        border:"none",
+                        borderRadius:"10px",
+                        color:"white"
+                      }}>Lets together work</button>
+                    </div>
+                    <div className="services-mid-side">
+                      <div className="mid-services-block1">
+                        <p className='correct2'>Ideate</p>
+                        <ul className='mid-link'>
+                          <Link to="/ui"><li className='correct'>UX & UI Design Services </li></Link>
+                          <Link to="/software"><li>Software Development Services</li></Link>
+                          <Link to="/cloud"><li>Cloud Consulting & Services</li></Link>
+                        </ul>
+                      </div>
 
+                      <div className="mid-services-block2">
+                        <p>Improve</p>
+                        <ul className='mid-link'>
+                          <Link to="/digitaltransformation"><li className='correct'>Digital Transformation Services</li></Link>
+                          <Link to="/dataengineering"><li >Data Engineering and Data Science Services</li></Link>
+                          <Link to="/mobile"><li  >Mobile App Development Services</li></Link>
+                          <Link to="/ai"><li  >AI & ML Services</li></Link>
+                        </ul>
+                      </div>
+                      <div className="mid-services-block3">
+                        <p>Scale</p>
+                        <ul className='mid-link'>
+                          <Link to="/nearshoringservices"><li className='correct'>Nearshoring Services for the North American Market</li></Link>
+                          <Link to="/software"><li>Software Development Outsourcing Services</li></Link>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="services-rigth-side">
+                      <div className="right-side-block1">
+                        <p className='correct2'>Build</p>
+                        <ul className='mid-link'>
+                          <Link to='/devlopment_team'><li className='correct'>Dedicated Software Development Teams</li></Link>
+                          <Link to="/cloud"><li>Cloud Engineering Service</li></Link>
+                          <Link to="/embeddedsoftware"><li>Embedded Software Services</li></Link>
+                          <Link to="/mobile"><li>Mobile Apps Development Services</li></Link>
+                        </ul>
+                      </div>
+                      <div className="right-side-block2">
+                        <p>Operate</p>
+                        <ul className='mid-link'>
+                          <Link to="/IdentityandAccess"><li className='correct'>Identity and Access Management Services</li></Link>
+                          <Link to="/automation"><li>Automation Testing Services</li></Link>
+                          <Link to="/devops"><li>DevOps Consulting Services & Solutions</li></Link>
+                          <Link to="/softwareauditing"><li>Software Auditing Services</li></Link>
+                          <Link to="/securityaudit"><li>Security Audit and Governance Services</li></Link>
+                        </ul>
+                      </div>
+                    </div>
                   </div>
-                  <div className="services-mid-side">
-                    <div className="mid-services-block1">
-                      <p className='correct2'>Ideate</p>
-                      <ul className='mid-link'>
-                        <Link to="/ui"><li className='correct'>UX & UI Design Services </li></Link>
-                        <Link to="/software"><li >Software Development Services</li></Link>
-                        <Link to="/cloud"><li >Cloud Consulting & Services</li></Link>
-                      </ul>
-                    </div>
+                </div>
+              </li>
 
-                    <div className="mid-services-block2">
-                      <p>Improve</p>
-                     <ul className='mid-link'>
-                      <Link to="/digitaltransformation"><li className='correct'>Digital Transformation Services</li></Link>
-                      <Link to="/dataengineering"><li >Data Engineering and Data Science Services</li></Link>
-                      <Link to="/mobile"><li  >Mobile App Development Services</li></Link>
-                      <Link to="/ai"><li  >AI & ML Services</li></Link>
-                     
-                     </ul>
-
-
-                    </div>
-                    <div className="mid-services-block3">
-                      <p>Scale</p>
-                      <ul className='mid-link'>
-                       <Link to="/nearshoringservices"><li className='correct'>Nearshoring Services for the North American Market</li></Link>
-                        <Link to="/software"><li>Software Development Outsourcing Services</li></Link>
-                       
-                      </ul>
-                    </div>
-
-
-                  </div>
-                  <div className="services-rigth-side">
-                   <div className="right-side-block1">
-                   <p className='correct2'>Build</p>
-                    <ul className='mid-link'>
-                     <Link to='/devlopment_team'> <li className='correct'>Dedicated Software Development Teams</li></Link>
-                     <Link to="/cloud"><li>Cloud Engineering Service</li></Link>
-                      <Link to="/embeddedsoftware"><li>Embedded Software Services</li></Link>
-                     <Link to="/mobile"> <li>Mobile Apps Development Services</li></Link>
+              {/* Products Link with Hover */}
+              <li 
+                className="nav-item"
+                onMouseEnter={() => setIsHoveringProduct(true)} 
+                onMouseLeave={() => setIsHoveringProduct(false)}
+              >
+                <Link className="nav-link text-white" to="#">Products</Link>
+                <div className={`products-dropdown ${isHoveringProduct ? 'show' : ''}`}>
+                  <div className="products-menu">
+                    <ul>
+                      <li>Our Apps</li>
+                      <li>Our Websites</li>
+                      <li>Our Services</li>
+                      <li>Our Ai Ml Products</li>
                     </ul>
-                   </div>
-                   <div className="rigth-side-block2">
-                    <p>Operate</p>
-                    <ul className='mid-link'>
-                      <Link to="/IdentityandAccess"><li className='correct'>Identity and Access Management Services</li></Link>
-                     <Link to="/automation"><li>Automation Testing Services</li></Link>
-                      <Link to="/devops"><li>DevOps Consulting Services & Solutions</li></Link>
-                      <Link to="/softwareauditing"><li>Software Auditing Services</li></Link>
-                      <Link to="/securityaudit"><li>Security Audit and Governance Services</li></Link>
+                  </div>
+                </div>
+              </li>
+
+              {/* Industries Link with Hover */}
+              <li 
+                className="nav-item"
+                onMouseEnter={() => setIsHoveringIndustries(true)} 
+                onMouseLeave={() => setIsHoveringIndustries(false)}
+              >
+                <Link className="nav-link text-white" to="/industries">Industries</Link>
+                <div className={`industries-dropdown ${isHoveringIndustries ? 'show' : ''}`}>
+                  <div className="industries-menu">
+                    <ul>
+                      <li>Financial Services</li>
+                      <li>Telecom</li>
+                      <li>Biotech & Healthcare</li>
+                      <li>Media & Entertainment</li>
+                      <li>Sports Betting</li>
+                      <li>Manufacturing</li>
                     </ul>
-                   </div>
                   </div>
-                  </div>
-                  .
                 </div>
               </li>
 
               <li className="nav-item">
-                <a className="nav-link text-white" href="#">Product</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white" href="#">Industries</a>
-              </li>
-              <li className="nav-item">
-                <button className="button btn-primary">Sign In</button>
+                <button className="signIn">Sign In</button>
               </li>
             </ul>
           </div>
